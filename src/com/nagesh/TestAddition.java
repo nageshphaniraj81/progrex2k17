9ae2d699415193ff6015e2c80cd1e6c7884e62c4
Nagesh Phaniraj <nageshphaniraj81@gmail.com>
2017-01-23 22:08:35 +0530

    Adding test addition class to git

---
+++ src/com/nagesh/TestAddition.java
@@ -0,0 +1,10 @@
+package com.nagesh;
+
+/**
+ * Created by Nagesh Phaniraj on 1/23/2017.
+ */
+public class TestAddition {
+    public static void main(String[] Args){
+        System.out.println("Second class");
+    }
+}
