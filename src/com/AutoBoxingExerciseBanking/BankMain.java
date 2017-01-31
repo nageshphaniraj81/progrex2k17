495866e913c8aff2da7037baf0c50707b3209b7e
Nagesh Phaniraj <nageshphaniraj81@gmail.com>
2017-01-31 23:33:41 +0530

    Adding AutoBoxing and Unboxing Exercise code

--- src/com/AutoBoxingExerciseBanking/BankMain.java
+++ src/com/AutoBoxingExerciseBanking/BankMain.java
@@ -4,6 +4,18 @@ package com.AutoBoxingExerciseBanking;
  * Created by Nagesh Phaniraj on 1/31/2017.
  */
 public class BankMain {
+    public static void main(String [] args){
+        Bank bank = new Bank("State Bank of India");
+        bank.addBranch("Belandur");
+        bank.addCustomer("Belandur","Nagesh",99.90);
+        bank.addCustomer("Belandur","Anusha",300.50);
+        bank.addBranch("Jayanagar");
+        bank.addCustomer("Jayanagar","Santosh",50.80);
 
-    
+        bank.addCustomerTransaction("Belandur","Nagesh",44.00);
+        bank.addCustomerTransaction("Belandur","Nagesh",999.00);
+        bank.addCustomerTransaction("Belandur","Anusha",50.00);
+        bank.listCustomer("Belandur",true);
+        bank.listCustomer("Jayanagar",true);
+    }
 }
